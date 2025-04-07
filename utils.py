c49a439baa06574e24453c9e44c93bb40361f6db
Pedro Barros de Souza Lima <102550244+pedrobslima@users.noreply.github.com>
2025-04-07 17:19:52 -0300

    Initial commit

---
+++ utils.py
@@ -0,0 +1,109 @@
+
+# ------ IMPORTS ------
+# Basic
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scikitplot as skplt
+from random import randint
+
+# Preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import RandomOverSampler
+
+# MLFlow
+import joblib
+import mlflow
+import mlflow.sklearn
+import logging
+
+# Base Models (+ ST assessor)
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.linear_model import SGDClassifier, SGDRegressor
+from sklearn.linear_model import LogisticRegression, ElasticNet # LogReg->Classf. | ElasticNet->Reg. 
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+from sklearn.svm import SVC, SVR # kernels: 'linear', 'poly' e 'rbf'
+from sklearn.neural_network import MLPClassifier, MLPRegressor
+from kan import KANClassifier, KANRegressor
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.ensemble import GradientBoostingClassifier, GradientBoostingRegressor
+from sklearn.ensemble import AdaBoostClassifier, AdaBoostRegressor
+from xgboost import XGBClassifier, XGBRegressor
+
+# Multi-Target Assessors
+from sklearn.multioutput import MultiOutputRegressor, RegressorChain
+
+# Performance eval
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, roc_auc_score, average_precision_score
+from sklearn.metrics import mean_squared_error as mse, mean_absolute_error as mae, root_mean_squared_error as rmse
+#from sklearn.inspection import PartialDependenceDisplay as pdp
+#from sklearn.inspection import permutation_importance
+
+# ------ FUNCTIONS ------
+def extract_final_losses(history):
+    """Função para extrair o melhor loss de treino e validação.
+    
+    Argumento(s):
+    history -- Objeto retornado pela função fit do keras.
+    
+    Retorno:
+    Dicionário contendo o melhor loss de treino e de validação baseado 
+    no menor loss de validação.
+    """
+    train_loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    idx_min_val_loss = np.argmin(val_loss)
+    return {'train_loss': train_loss[idx_min_val_loss], 'val_loss': val_loss[idx_min_val_loss]}
+
+def plot_training_error_curves(history):
+    """Função para plotar as curvas de erro do treinamento da rede neural.
+    
+    Argumento(s):
+    history -- Objeto retornado pela função fit do keras.
+    
+    Retorno:
+    A função gera o gráfico do treino da rede e retorna None.
+    """
+    train_loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    
+    fig, ax = plt.subplots()
+    ax.plot(train_loss, label='Train')
+    ax.plot(val_loss, label='Validation')
+    ax.set(title='Training and Validation Error Curves', xlabel='Epochs', ylabel='Loss (MSE)')
+    ax.legend()
+    plt.show()
+
+def compute_performance_metrics(y, y_pred_class, y_pred_scores=None):
+    accuracy = accuracy_score(y, y_pred_class)
+    recall = recall_score(y, y_pred_class, zero_division=0)
+    precision = precision_score(y, y_pred_class, zero_division=0)
+    f1 = f1_score(y, y_pred_class, zero_division=0)
+    performance_metrics = (accuracy, recall, precision, f1)
+    if y_pred_scores is not None:
+        auroc = roc_auc_score(y, y_pred_scores)
+        aupr = average_precision_score(y, y_pred_scores)
+        performance_metrics = performance_metrics + (auroc, aupr)
+    return performance_metrics
+
+def plot_ks(y, y_pred_proba):
+    skplt.metrics.plot_ks_statistic(y, y_pred_proba)
+    plt.show()
+    y_pred_proba = y_pred_proba[:, 1]
+    auroc = roc_auc_score(y, y_pred_proba)
+    aupr = average_precision_score(y, y_pred_proba)
+    return auroc, aupr
+
+def print_metrics_summary(accuracy, recall, precision, f1, auroc=None, aupr=None):
+    print()
+    print("{metric:<18}{value:.4f}".format(metric="Accuracy:", value=accuracy))
+    print("{metric:<18}{value:.4f}".format(metric="Recall:", value=recall))
+    print("{metric:<18}{value:.4f}".format(metric="Precision:", value=precision))
+    print("{metric:<18}{value:.4f}".format(metric="F1:", value=f1))
+    if auroc is not None:
+        print("{metric:<18}{value:.4f}".format(metric="AUROC:", value=auroc))
+    if aupr is not None:
+        print("{metric:<18}{value:.4f}".format(metric="AUPR:", value=aupr))
